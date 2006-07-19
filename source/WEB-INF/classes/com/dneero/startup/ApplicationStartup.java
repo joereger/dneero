198ba1ff8c248b1af515159227ade993ba14fea0
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-07-19 19:16:03 +0000

    Added the Quartz scheduling framework.

--- source/WEB-INF/classes/com/dneero/startup/ApplicationStartup.java
+++ source/WEB-INF/classes/com/dneero/startup/ApplicationStartup.java
@@ -8,9 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
+import org.quartz.Scheduler;
+import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.ee.servlet.QuartzInitializerServlet;
 
 /**
  * User: Joe Reger Jr
@@ -18,6 +22,8 @@ import org.apache.log4j.Logger;
  * Time: 10:50:54 AM
  */
 public class ApplicationStartup extends HttpServlet {
+
+    //@todo implement ApplicationStartup as a ServletContextListener for better app server portability
 
     Logger logger = Logger.getLogger(this.getClass().getName());
 
@@ -30,6 +36,7 @@ public class ApplicationStartup extends HttpServlet {
         }
 
         public void init(ServletConfig config){
+            //Configure some dir stuff
             WebAppRootDir ward = new WebAppRootDir(config);
             HibernateUtil.getSession();
             logger.info("dNeero Application started!  Let's make some dinero!  WebAppRootDir = " + WebAppRootDir.getWebAppRootPath());
