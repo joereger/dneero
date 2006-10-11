961e94b74048412263c0e0f8e1be71dd24f42115
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-10-11 18:50:58 +0000

    Paypal and Verisign dirs.

---
+++ source/WEB-INF/classes/com/dneero/money/paypal/Pay.java
@@ -0,0 +1,16 @@
+package com.dneero.paypal;
+
+import com.dneero.dao.User;
+
+/**
+ * User: Joe Reger Jr
+ * Date: Sep 25, 2006
+ * Time: 9:52:40 AM
+ */
+public class Pay {
+
+    public static void pay(String paypalAddress, double amt){
+
+    }
+
+}
