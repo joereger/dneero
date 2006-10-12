a0f9403fe4928c4f995defcc0c9a13781cc8fb67
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2006-10-12 13:45:55 +0000

    Paypal code.

--- source/WEB-INF/classes/com/dneero/money/paypal/Charge.java
+++ source/WEB-INF/classes/com/dneero/money/paypal/Charge.java
@@ -1,6 +1,6 @@
-package com.dneero.paypal;
+package com.dneero.money.paypal;
 
-import com.dneero.dao.User;
+import org.apache.log4j.Logger;
 
 /**
  * User: Joe Reger Jr
@@ -9,8 +9,57 @@ import com.dneero.dao.User;
  */
 public class Charge {
 
-    public static void charge(String paypalAddress, double amt){
+    public static void charge(String paypalAddress, double amt) throws Exception {
+
+        Logger logger = Logger.getLogger(Charge.class);
+        logger.error("PayPal can't be used to charge");
+
 
     }
+
+//        DoDirectPaymentRequestType request = new DoDirectPaymentRequestType();
+//        DoDirectPaymentRequestDetailsType details = new DoDirectPaymentRequestDetailsType();
+//
+//        CreditCardDetailsType creditCard = new CreditCardDetailsType();
+//        creditCard.setCreditCardNumber("4721930402892796");
+//        creditCard.setCreditCardType(CreditCardTypeType.Visa);
+//        creditCard.setCVV2("000");
+//        creditCard.setExpMonth(11);
+//        creditCard.setExpYear(2007);
+//
+//        PayerInfoType cardOwner = new PayerInfoType();
+//        cardOwner.setPayerCountry(CountryCodeType.US);
+//
+//
+//        AddressType address = new AddressType();
+//        address.setPostalCode("95101");
+//        address.setStateOrProvince("CA");
+//        address.setStreet1("123 Main St");
+//        address.setCountryName("US");
+//        address.setCountry(CountryCodeType.US);
+//        address.setCityName("San Jose");
+//        cardOwner.setAddress(address);
+//
+//        PersonNameType payerName = new PersonNameType();
+//        payerName.setFirstName("SDK");
+//        payerName.setLastName("Buyer");
+//        cardOwner.setPayerName(payerName);
+//
+//        creditCard.setCardOwner(cardOwner);
+//        details.setCreditCard(creditCard);
+//
+//        details.setIPAddress("12.36.5.78");
+//        details.setMerchantSessionId("456977");
+//        details.setPaymentAction(PaymentActionCodeType.Sale);
+//
+//        PaymentDetailsType payment = new PaymentDetailsType();
+//
+//        BasicAmountType orderTotal = new BasicAmountType();
+//        orderTotal.setCurrencyID(CurrencyCodeType.USD);
+//        orderTotal.set_value(Str.formatForMoney(amt));
+//        payment.setOrderTotal(orderTotal);
+//
+//        details.setPaymentDetails(payment);
+//        request.setDoDirectPaymentRequestDetails(details);
 
 }
--- source/WEB-INF/classes/com/dneero/money/paypal/Pay.java
+++ source/WEB-INF/classes/com/dneero/money/paypal/Pay.java
@@ -1,6 +1,9 @@
-package com.dneero.paypal;
+package com.dneero.money.paypal;
 
-import com.dneero.dao.User;
+import com.dneero.util.Str;
+import com.paypal.sdk.services.CallerServices;
+import com.paypal.soap.api.*;
+import org.apache.log4j.Logger;
 
 /**
  * User: Joe Reger Jr
@@ -9,8 +12,42 @@ import com.dneero.dao.User;
  */
 public class Pay {
 
-    public static void pay(String paypalAddress, double amt){
+    public static void pay(String paypalAddress, double amt) throws Exception{
+        Logger logger = Logger.getLogger(Charge.class);
+        CallerFactory callerFactory = new CallerFactory();
+        CallerServices caller = callerFactory.getCaller();
 
+        MassPayRequestType request = new MassPayRequestType();
+        MassPayRequestItemType mprit = new MassPayRequestItemType();
+
+        mprit.setReceiverEmail(paypalAddress);
+
+        BasicAmountType orderTotal = new BasicAmountType();
+        orderTotal.setCurrencyID(CurrencyCodeType.USD);
+        orderTotal.set_value(Str.formatForMoney(amt));
+        mprit.setAmount(orderTotal);
+
+        MassPayRequestItemType[] mpArray = new MassPayRequestItemType[1];
+        mpArray[0] = mprit;
+        request.setMassPayItem(mpArray);
+
+        try{
+            MassPayResponseType response = (MassPayResponseType) caller.call("MassPay", request);
+            logger.debug("Operation Ack: " + response.getAck());
+            ErrorType[] errors = response.getErrors();
+            if (errors!=null){
+                for (int i = 0; i < errors.length; i++) {
+                    ErrorType error = errors[i];
+                    logger.debug("Error "+i);
+                    logger.debug(error.getLongMessage());
+                }
+            }
+            logger.debug("---------- Results ----------");
+            logger.debug("getCorrelationID(): " + response.getCorrelationID());
+            //logger.debug("Gross Amount: " + response.getDoExpressCheckoutPaymentResponseDetails().getPaymentInfo().getGrossAmount().getCurrencyID()+ " " + response.getDoExpressCheckoutPaymentResponseDetails().getPaymentInfo().getGrossAmount().get_value());
+        } catch (Exception ex){
+            logger.error(ex);
+        }
     }
 
 }
